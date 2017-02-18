4d98ed50dfa2ec4ccde31e212ffb589eb59a67ee
Michael Redford <michaeliredford@gmail.com>
2017-02-18 14:07:54 +0100

    config file. TODO: add complete credits finalize resolution.

---
+++ conf.lua
@@ -0,0 +1,12 @@
+
+function love.conf(t)
+  t.window.width = 1280
+	t.window.height = 600
+	t.author = "Michael Redford (@MIfeanyi)"	
+	t.title = "Love Joust"
+	t.window.vsync = false
+	t.window.fullscreen= false
+	t.window.fsaa = 0
+	t.console = true
+	t.release = false
+end
