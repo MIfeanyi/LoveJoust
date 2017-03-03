4a102496e3b810e4884c5b2a563fe10c5aa031e3
Michael Redford <michaeliredford@gmail.com>
2017-03-04 00:04:37 +0100

    prototype

--- states/ui.lua
+++ states/ui.lua
@@ -1,15 +1,23 @@
 
 ui = {lives = 3, score = 0,
- background = {x,y,h,w},
+ bg = {x,y,h,w},
+ color = {r,b,g,a}
  images = {hp},
- 
+ table ={} --to store highscores 
  }
-
+--[[TODO: io functions
 function ui:save()
-end
-
-function ui:draw()
 end
 
 function ui:load()
 end
+
+function ui:update() -- update score table
+end
+]]
+function ui:draw()
+love.graphics.setColor(self.color.r,self.color.b,self.color.g,self.color.a)
+love.graphics.rectangle("fill", self.bg.x, self.bg.y, self.bg.h, self.bg.w)
+love.graphics.setColor(255,255,255,255)
+end
+
